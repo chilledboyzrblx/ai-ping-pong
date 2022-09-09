c6c5cded78aafdc06b2a875e1134e73ae17eef18
Mr.GeschVed <86187817+chilledboyzrblx@users.noreply.github.com>
2022-09-09 14:40:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,6 +24,11 @@ rightWristY = 0;
 rightWristscore = 0;
 game_status = "";
 
+function preload(){
+  ball_touch_sound = loadSound('ball_touch_paddel.wav');
+  ballMissed = loadSound('missed.wav');
+}
+
 function setup() {
   var canvas = createCanvas(700, 500);
   canvas.center();
@@ -71,7 +76,7 @@ function draw() {
     fill(250, 0, 0);
     stroke(0, 0, 250);
     strokeWeight(0.5);
-    paddle1Y = mouseY;
+    paddle1Y = rightWristY;
     rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
 
 
@@ -156,10 +161,12 @@ function move() {
   if (ball.x - 2.5 * ball.r / 2 < 0) {
     if (ball.y >= paddle1Y && ball.y <= paddle1Y + paddle1Height) {
       ball.dx = -ball.dx + 0.5;
+      ball_touch_sound.play();
     } else {
       pcscore++;
       reset();
       navigator.vibrate(100);
+      ballMissed.play();
     }
   }
   if (pcscore == 4) {
@@ -170,7 +177,7 @@ function move() {
     stroke("white");
     textSize(25)
     text("Game Over!☹☹", width / 2, height / 2);
-    text("Reload The Page!", width / 2, height / 2 + 30)
+    text("Press Restart to play again!", width / 2, height / 2 + 30)
     noLoop();
     pcscore = 0;
   }
@@ -199,4 +206,11 @@ function paddleInCanvas() {
   if (mouseY < 0) {
     mouseY = 0;
   }
+}
+
+function restart(){
+  pcscore = 0;
+  playerscore = 0;
+  game_status = "start"
+  document.getElementById('status').innerHTML = "Status: Game is Starting!";
 }
